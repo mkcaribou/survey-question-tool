c24afdcd2acf2dae019b741fa132697ff167419b
mkcaribou <marius@cariboudigital.net>
2025-02-12 15:33:29 -0600

    Craco tailwind config

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import QuestionsDatabase from './QuestionsDatabase';
 
 function App() {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-center text-2xl font-bold my-4">survey questions</h1>
-      <QuestionsDatabase />
-    </div>
   );
 }
 
