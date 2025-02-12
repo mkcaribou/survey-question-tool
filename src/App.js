3921e691bb4e636cc8af96734bc4b1d31ab24c31
mkcaribou <marius@cariboudigital.net>
2025-02-12 15:36:31 -0600

    Another crack

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import QuestionsDatabase from './QuestionsDatabase';
 
 function App() {
   return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Survey Questions Database</h1>
+    </div>
   );
 }
 
