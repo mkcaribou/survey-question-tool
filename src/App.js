0b9477260b57f36601bf1efda3aa8bd26affb985
mkcaribou <marius@cariboudigital.net>
2025-02-12 15:44:54 -0600

    again

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import QuestionsDatabase from './QuestionsDatabase';
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">Survey Questions Database</h1>
+    <div className="min-h-screen flex flex-col items-center justify-start bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 mt-4">Survey Questions Database</h1>
+      <QuestionsDatabase />
     </div>
   );
 }
